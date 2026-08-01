4d4797f8f76c1fb5ea6371c2beacb13f786bf2fb
Chenchen <wu0127chen@163.com>
2026-08-01 21:34:40 +0800

    reduce_v3

---
+++ operators/reduce/reduce_v3.cu
@@ -0,0 +1,22 @@
+#include "reduce_v3.cuh"
+#include <device_launch_parameters.h>
+
+
+__global__ void reduce(float *d_input, float *d_output)
+{
+    __shared__ float shared[THREAD_PER_BLOCK];
+    float *input_begin = d_input + blockDim.x * blockIdx.x;
+    shared[threadIdx.x] = input_begin[threadIdx.x];
+    __syncthreads();
+
+    for (int i = blockDim.x / 2; i > 0; i /= 2)
+    {
+        if (threadIdx.x < i)
+        {
+            shared[threadIdx.x] += shared[threadIdx.x + i];
+        }
+        __syncthreads();
+    }
+    if (threadIdx.x == 0)
+        d_output[blockIdx.x] = shared[0];
+}
